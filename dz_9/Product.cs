0d8213190b1fa7f121834fa0fda581948e282c98
Vlad <vladvesinin676@gmail.com>
2025-03-27 18:57:01 +0500

    commit

---
+++ dz_9/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_9
+{
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(int id, string name, decimal price, string category, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ dz_9/Store.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_9
+{
+    public class Store<T> : IStore<T> where T : IProduct
+    {
+        private List<T> product_list_ = new List<T>();
+
+
+        public void Add(T product)
+        {
+            if (!product_list_.Any(i => i.Id == product.Id))
+            {
+                product_list_.Add(product);
+            }
+            else
+            {
+                throw new Exception("ERROR\nТовар с таким Id уже есть.");
+            }
+        }
+
+
+        public void Remove(int id)
+        {
+            var storeRemove = product_list_.FirstOrDefault(i => i.Id == id);
+            if (storeRemove != null)
+            {
+                product_list_.Remove(storeRemove);
+            }
+            else
+            {
+                throw new InvalidOperationException("ERROR\nТовара с таким Id нет.");
+            }
+        }
+
+
+        public T GetProductById(int id)
+        {
+            var item = product_list_.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                return item;
+            }
+            else
+            {
+                throw new Exception("ERROR\nТовара с таким Id нет.");
+            }
+        }
+
+
+        public void UpdatePrice(int id, decimal newPrice)
+        {
+            var item = product_list_.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                item.Price = newPrice;
+            }
+            else
+            {
+                throw new ArgumentException("ERROR\nТовара с таким Id нет.");
+            }
+        }
+
+
+        public void UpdateQuantity(int id, int newQuantity)
+        {
+            var item = product_list_.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                item.Quantity = newQuantity;
+            }
+            else
+            {
+                throw new ArgumentException("ERROR\nТовара с таким Id нет.");
+            }
+        }
+
+
+        public List<T> GetProductsByCategory(string category)
+        {
+            return product_list_.Where(i => i.Category == category).ToList();
+        }
+
+
+        public Dictionary<string, List<T>> GroupByCategory()
+        {
+            Dictionary<string, List<T>> groups = new Dictionary<string, List<T>>();
+            var result = product_list_.GroupBy(i => i.Category);
+
+            foreach (var i in result)
+            {
+                groups[i.Key] = i.ToList();
+            }
+
+            return groups;
+        }
+    }
+}
